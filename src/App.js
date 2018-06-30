f7c25f6b923c54a4f12cc45642cadb222c86c7a3
Jordan Keen <jordankeen.7@gmail.com>
2018-06-30 13:07:27 -0400

    setup of note and noteform folders
    - css and jsx files for note/noteform

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Note from './Note/Note';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+	super(props);
+
+	// we're going to setup the react state of our component
+	this.state = {
+	  notes: [
+	  	{ id: 1, noteContent: "Note 1 here" },
+	  	{ id: 2, noteContent: "Note 2 here" },
+	  ],
+	}
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+	return (
+	  <div className="notesWrapper">
+	  	<div className="notesHeader">
+	  		<h1 className="heading">React & Firebase To-Do List</h1>
+	  	</div>
+	  	<div className="notesBody">
+	  	{
+	  		this.state.notes.map((note) => {
+	  			return(
+					<Note noteContent={note.noteContent} noteId={note.id} key={note.id}/>
+	  			)
+	  		})
+	  	}
+		</div>
+		<div className="notesFooter">
+			footer here
+		</div>
+	  </div>
+	);
   }
 }
 
