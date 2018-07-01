40029d888eeb930175a76e2389680d89986f1162
Jordan Keen <jordankeen.7@gmail.com>
2018-07-01 14:29:27 -0400

    integration of firebase
    - added firebase
    - function to remove note

---
+++ src/Config/Config.js
@@ -0,0 +1,9 @@
+export const  DB_CONFIG = {
+	apiKey: "AIzaSyDDquK-XB-qKhtFxVjiWZNM8KQqUrMaJ7E",
+	authDomain: "react-notes-8bfc5.firebaseapp.com",
+	databaseURL: "https://react-notes-8bfc5.firebaseio.com",
+	projectId: "react-notes-8bfc5",
+	storageBucket: "react-notes-8bfc5.appspot.com",
+	messagingSenderId: "968915443233"
+};
+ 
